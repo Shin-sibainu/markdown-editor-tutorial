4721328b5d0b03b57c8d33c05e8a84eedcf3a704
Shin-sibainu <shincode0712@gmail.com>
2022-02-02 18:09:04 +0900

    alldone

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
 var simplemde = new SimpleMDE({
   element: document.getElementById("editor"),
   forceSync: true,
-  spellChecker: true,
+  spellChecker: false,
 });
 // console.log(simplemde);
 
 const markdown = document.getElementById("editor");
 
 function draw_preview() {
-  const markdown_value = markdown.value;
+  // const markdown_value = markdown.value;
   const html = SimpleMDE.prototype.markdown(simplemde.value());
   document.getElementById("preview").innerHTML = html;
-  document.getElementById("html-preview").innerText = html;
+  // document.getElementById("html-preview").innerText = html;
   console.log(html);
 }
 
 document.addEventListener("input", () => {
-  //   console.log("a");
+  //console.log("a");
   draw_preview();
 });
